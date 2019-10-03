e0eacfa7d5c2d201a0054ab75529584950e0374c
kristya <shiriagina@yahoo.com>
2019-10-03 11:58:22 +0200

    added the main class and controller

---
+++ src/main/java/org/beauty/bea/Controller.java
@@ -0,0 +1,13 @@
+package org.beauty.bea;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+@RequestMapping("/hello")
+    public String sayHi(){
+       return "Hi, I am a new application:) !";
+    }
+}
