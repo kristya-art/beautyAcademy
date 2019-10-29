88fd6d8ccfb639b22235940e15564adfd3e8ed07
kristya <shiriagina@yahoo.com>
2019-10-30 00:15:51 +0100

    added system diagram for spring secirity login

--- src/main/java/org/beauty/bea/controller/AuthController.java
+++ src/main/java/org/beauty/bea/controller/AuthController.java
@@ -28,7 +28,7 @@ import java.util.Set;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
-public class AuthRestAPIs {
+public class AuthController {
 
 	@Autowired
 	AuthenticationManager authenticationManager;
