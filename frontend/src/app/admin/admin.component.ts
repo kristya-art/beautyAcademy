7915287fe2c97888a0e617de9fcce71978d7a5bf
kristya <shiriagina@yahoo.com>
2019-11-05 12:02:31 +0100

    added implementation to some components and services

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  board: string;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getAdminBoard().subscribe(
+      data => {
+        this.board = data;
+      },
+      error => {
+        this.errorMessage = `${error.status}: ${JSON.parse(error.error).message}`;
+      }
+    );
   }
-
 }
--- frontend/src/app/auth/signup-info.ts
+++ frontend/src/app/auth/signup-info.ts
@@ -7,11 +7,11 @@ export class SignUpInfo {
   password: string;
 
 
-  constructor(name: string, username: string, email: string, role: string[], password: string) {
+  constructor(name: string, username: string, email: string, password: string) {
     this.name = name;
     this.username = username;
     this.email = email;
-    this.role = role;
+    this.role = ['user'];
     this.password = password;
   }
 }
