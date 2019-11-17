ea3fcd6f6a8c30ed910da3b2c55b92b0061da719
kristya <shiriagina@yahoo.com>
2019-11-18 00:10:55 +0100

    added filter for messages

--- src/main/java/org/beauty/bea1/repository/MessageRepo.java
+++ src/main/java/org/beauty/bea1/repository/MessageRepo.java
@@ -3,5 +3,9 @@ package org.beauty.bea1.repository;
 import org.beauty.bea1.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message,Long> {
+ List<Message> findByTag(String tag);
+
 }
