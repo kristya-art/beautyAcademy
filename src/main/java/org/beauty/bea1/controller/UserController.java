5f541a03610064665bb154dc807633b1215b5531
kristya <shiriagina@yahoo.com>
2019-11-19 13:27:15 +0100

    added UserController ahd several views

---
+++ src/main/java/org/beauty/bea1/controller/UserController.java
@@ -0,0 +1,34 @@
+package org.beauty.bea1.controller;
+
+import org.beauty.bea1.domain.Role;
+import org.beauty.bea1.domain.User;
+import org.beauty.bea1.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model){
+        model.addAttribute("users",userRepo.findAll());
+
+        return "userList";
+    }
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user",user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+
+}
+
---
+++ src/main/resources/templates/main.ftl
@@ -0,0 +1,32 @@
+<#import "parts/common.ftl" as c>
+<#import "parts/login.ftl" as l>
+
+<@c.page>
+    <div>
+        <@l.logout />
+        <span><a href="/user">User list</span>
+    </div>
+    <div>
+        <form method="post">
+            <input type="text" name="text" placeholder="Введите сообщение" />
+            <input type="text" name="tag" placeholder="Тэг">
+            <input type="hidden" name="_csrf" value="${_csrf.token}" />
+            <button type="submit">Add</button>
+        </form>
+    </div>
+    <div>List of messages</div>
+    <form method="get" action="/main">
+        <input type="text" name="filter" value="${filter}">
+        <button type="submit">Search</button>
+    </form>
+    <#list messages as message>
+        <div>
+            <b>${message.id}</b>
+            <span>${message.text}</span>
+            <i>${message.tag}</i>
+            <strong>${message.authorName}</strong>
+        </div>
+    <#else>
+        No message
+    </#list>
+</@c.page>
