a94c7158caed0826816ab82b4d8242d5a6d6c99d
kristya <shiriagina@yahoo.com>
2019-11-19 13:54:16 +0100

    added method to user controller to change user name and role

--- src/main/java/org/beauty/bea1/controller/UserController.java
+++ src/main/java/org/beauty/bea1/controller/UserController.java
@@ -6,9 +6,12 @@ import org.beauty.bea1.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -29,6 +32,30 @@ public class UserController {
         return "userEdit";
     }
 
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String,String > form,
+            @RequestParam ("userId")User user)
+    {
+        user.setUsername(username);
 
-}
+       Set<String> roles = Arrays.stream(Role.values())
+               .map(Role::name)
+               .collect(Collectors.toSet());
+
+       user.getRoles().clear();
+       for(String key: form.keySet()){
+           if(roles.contains(key)){
+               user.getRoles().add(Role.valueOf(key));
+           }
+       }
+        userRepo.save(user);
+        return "redirect:/user";
+    }
+    }
+
+
+
+
 
