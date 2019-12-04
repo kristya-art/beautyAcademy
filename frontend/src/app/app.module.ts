dda1057a7f05ce3f9db7640dff62a28a9bd5efa0
kristya <shiriagina@yahoo.com>
2019-12-04 01:58:17 +0100

    updated angular and services

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from './core/material.module';
+
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { LecturerComponent } from './lecturer/lecturer.component';
+import { AdminComponent } from './admin/admin.component';
+
+import {ɵHttpInterceptingHandler} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    UserComponent,
+    LecturerComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [ɵHttpInterceptingHandler],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
